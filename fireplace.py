240ddf3a2545ccd63cc461efd2132a2b489f5391
Sean Graham <github@sean-graham.com>
2015-12-05 23:36:03 -0500

    linted the file

--- fireplace.py
+++ fireplace.py
@@ -6,24 +6,26 @@ import argparse
 import ConfigParser
 import logging
 
+
 class FireplaceControl(object):
 
     deviceId = ""
     accessToken = ""
 
-    def __init__(self,argv):
+    def __init__(self, argv):
         config = None
         logging.basicConfig(level=logging.WARNING)
 
-
-        parser = argparse.ArgumentParser(description='Control the virtual fireplace.')
+        parser = argparse.ArgumentParser(
+            description='Control the virtual fireplace.')
         parser.add_argument('--brightness', action='store',
-                           default=255, required=False, type=int,
-                           help='The brightness (0-255) (default: 255)')
+                            default=255, required=False, type=int,
+                            help='The brightness (0-255) (default: 255)')
         parser.add_argument('--off', action='store_true',
-                           help='Turn the strip off? (default: False)')
-        parser.add_argument('-v','--verbose', action='store_true',
-                           help='Turn on verbose debugging output? (default: False)')
+                            help='Turn the strip off? (default: False)')
+        parser.add_argument('-v', '--verbose', action='store_true',
+                            help='Turn on verbose debugging output? \
+                                    (default: False)')
 
         args = parser.parse_args()
 
@@ -45,8 +47,8 @@ class FireplaceControl(object):
             exit(-1)
             raise
 
-        self.deviceId = config.get("fireplace", "deviceId");
-        self.accessToken = config.get("fireplace", "accessToken");
+        self.deviceId = config.get("fireplace", "deviceId")
+        self.accessToken = config.get("fireplace", "accessToken")
 
         self.setBrightness(args.brightness)
 
@@ -56,26 +58,26 @@ class FireplaceControl(object):
             self.turnOn()
 
     def sendRequest(self, endpoint, value):
-        url = 'https://api.particle.io/v1/devices/{0}/{1}'.format(self.deviceId, endpoint)
-
+        url = 'https://api.particle.io/v1/devices/{0}/{1}'.format(
+            self.deviceId, endpoint)
 
         args = {
-                'access_token': self.accessToken,
-                'args': value 
-                }
-            
-        r = requests.post(url, data=args);
+            'access_token': self.accessToken,
+            'args': value
+        }
+
+        r = requests.post(url, data=args)
 
         return r
 
     def turnOn(self):
-        r = self.sendRequest("toggleLights", "true")
+        self.sendRequest("toggleLights", "true")
 
     def turnOff(self):
-        r = self.sendRequest("toggleLights", "false")
+        self.sendRequest("toggleLights", "false")
 
     def setBrightness(self, value):
-        r = self.sendRequest("brightness", str(value))
+        self.sendRequest("brightness", str(value))
 
 if __name__ == "__main__":
     fireplaceControl = FireplaceControl(sys.argv[1:])
